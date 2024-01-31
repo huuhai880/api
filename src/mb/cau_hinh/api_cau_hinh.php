c91330f534b8d20595eaafc32e5d86d331c95616
hainhGLX <hainh@ican.vn>
2024-01-31 12:40:55 +0700

    a

--- src/mb/cau_hinh/api_cau_hinh.php
+++ src/mb/cau_hinh/api_cau_hinh.php
@@ -121,8 +121,6 @@ if ($_POST["action"] === "cap_nhat_config_price") {
 
     $sql .= "END  WHERE id IN ($id_update)";
 
-    var_dump($sql);
-   
     // Execute the update query
     if ($sql_connector->get_query_result($sql)) {
         $response['success'] = 1;
